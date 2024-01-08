47aa8a0694b754bb3d187eaddbf7415e64739f2f
Kevin <50965376+tgodkev@users.noreply.github.com>
2024-01-08 01:44:41 -0500

    Add server-side slug-based post retrieval
    A new file src/routes/api/posts/[slug]/+server.ts has been implemented to handle server-side retrieval of blog posts based on their slugs. The methods readAllPosts and findPostBySlug have been introduced for this purpose. This new approach simplifies post searching and improves the application's performance by performing these operations server-side.

---
+++ src/routes/api/posts/[slug]/+server.ts
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import path from 'path';
+
+const postsDirectory = 'src/lib/posts';
+
+interface Post {
+    title: string;
+    excerpt: string;
+    date: string;
+    body: string;
+    image_gallery: [];
+    slug: string;
+
+}
+
+
+function readAllPosts(): Post[] {
+    const filenames = fs.readdirSync(postsDirectory);
+    return filenames.map(filename => {
+        const filePath = path.join(postsDirectory, filename);
+        const fileContents = fs.readFileSync(filePath, 'utf8');
+        return JSON.parse(fileContents);
+    });
+}
+
+function findPostBySlug(posts: Post[], slug: string): Post | undefined {
+    return posts.find(post => post.slug === slug);
+}
+
+export async function GET({params}): Promise<Response> {
+    const posts = readAllPosts();
+    const targetPost = findPostBySlug(posts, params.slug);
+
+    return new Response(JSON.stringify(targetPost), {
+        status: 200,
+        headers: {'Content-Type': 'application/json'}
+    });
+}
+
+export const prerender = true;
