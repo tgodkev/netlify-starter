31a924fd582085a2a2eea83f6dadf310d145ee13
Kevin <50965376+tgodkev@users.noreply.github.com>
2024-01-10 21:30:45 -0500

    about page started

---
+++ src/routes/about/+page.server.ts
@@ -0,0 +1,16 @@
+
+import { getAll } from '$lib/utils';
+
+export async function load() {
+    try {
+        const route = getAll('cms/about');
+       return {
+           route
+       }
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+        return {
+            props: { route: [] }  // Handle the error as needed
+        };
+    }
+}
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,10 @@
+
+<script lang="ts">
+    import type { PageData } from './$types';
+   export let data: PageData;
+    export let {route} = data;
+    $: ({route} = data);
+
+console.log(route, "this is the about route")
+</script>
+
